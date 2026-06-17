8937eb26c1168bf23fbcaf6d227e5d9ed0e9fa44
LABORATORIO FELIPE SEGOVIA <laboratorio.fs@ad.uisek.edu.ec>
2026-06-17 09:44:28 -0500

    Permite agregar repositorio

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,7 +1,44 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { IonButton, IonContent, IonHeader, IonInput, IonPage, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab2.css';
+import { RepositoryPayload } from '../interfaces/RepositoryPayload';
+import { createRepository } from '../services/GithubService';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 const Tab2: React.FC = () => {
+  const history = useHistory();
+  const [loading, setLoading] = React.useState<boolean>(false);
+
+  const repoFormData : RepositoryPayload = {
+    name: '',
+    description: '',
+  };
+
+  const setRepoName = (value: string) => {
+    repoFormData.name = value;
+    console.log('Repository name updated:', repoFormData.name);
+  };
+
+  const setRepoDescription = (value: string) => {
+    repoFormData.description = value;
+  };
+
+  const saveRepository = () => {
+    if (repoFormData.name.trim() === '') {
+      alert('El nombre del repositorio es obligatorio');
+      return;
+    }
+    setLoading(true);
+    createRepository(repoFormData).then(() => {
+      history.push('/tab1')
+    }).catch(() => {
+      alert('Error al crear repositorio');
+    }).finally(() => {
+      setLoading(false);
+    });
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -23,6 +60,8 @@ const Tab2: React.FC = () => {
             labelPlacement="floating"
             fill='outline'
             placeholder='nombre-repositorio'
+            value={repoFormData.name}
+            onIonChange={e => setRepoName(e.detail.value!)}
           ></IonInput>
           <IonTextarea
             className='form-field'
@@ -31,13 +70,16 @@ const Tab2: React.FC = () => {
             fill='outline'
             placeholder='Descripción del repositorio'
             rows={6}
+            value={repoFormData.description}
+            onIonChange={e => setRepoDescription(e.detail.value!)}
             autoGrow
           ></IonTextarea>
-          <IonButton className='form-field' expand='block' fill='solid'>
+          <IonButton className='form-field' expand='block' fill='solid'
+            onClick={saveRepository}>
             Crear Repositorio
           </IonButton>
         </div>
-
+        {loading && <LoadingSpinner isOpen={loading} />}
       </IonContent>
     </IonPage>
   );
--- src/services/GithubService.ts
+++ src/services/GithubService.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { Repository } from '../interfaces/Repository';
+import { RepositoryPayload } from '../interfaces/RepositoryPayload';
 
 const GITHUB_API_BASE_URL = import.meta.env.VITE_GITHUB_API_URL || 'https://api.github.com';
 const GITHUB_API_TOKEN = import.meta.env.VITE_GITHUB_API_TOKEN;
@@ -8,18 +9,34 @@ export const fetchRepositories = async (): Promise< Repository[] > => {
     try {
         const response = await axios.get(`${GITHUB_API_BASE_URL}/user/repos`, {
             headers: {
-                Authorization: `token ${GITHUB_API_TOKEN}`,
+                'Authorization': `token ${GITHUB_API_TOKEN}`,
             },
             params: {
                 per_page: 100,
                 sort: 'created',
                 direction: 'desc',
                 affiliation: 'owner',
+                t: Date.now()
             },
         });
         return response.data as Repository[];
     } catch (error) {
         console.error('Error fetching repositories:', error);
         return [];
+    }
+}
+
+export const createRepository = async (repository: RepositoryPayload): Promise<Repository | null> => {
+    try {
+        const response = await axios.post(`${GITHUB_API_BASE_URL}/user/repos`, repository, {
+            headers: {
+                Authorization: `token ${GITHUB_API_TOKEN}`,
+            },
+        });
+        console.log('Repository created successfully:', response.data);
+        return response.data as Repository;
+    } catch (error) {
+        console.error('Error al crear repositorio:', error);
+        return null;
     }
 }
